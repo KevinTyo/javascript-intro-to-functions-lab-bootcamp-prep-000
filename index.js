2b62b695ca4c84ca8368887d33218463d785bd71
kevin.d.tyo@gmail.com <kevin.d.tyo@gmail.com>
2018-08-09 23:27:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string) {
   if (string ==== 
   string.toLowerCase()) {
   return  "I can't hear you"}
-  if else (string === string.toUpperCase()) {
+  else if (string === string.toUpperCase()) {
   return "YES, INDEED!"}
   else (string === "I love you, Grandma "){
   return "I love you, too"}
